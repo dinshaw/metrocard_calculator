cb9d7e25d21920f6361bb31ef5e433c08b16c423
Dinshaw Gobhai <gobhai@gmail.com>
2014-01-31 01:56:50 -0500

    add kiosk

--- card/card.go
+++ card/card.go
@@ -1,10 +1,5 @@
 package metrocard
 
 type Card struct {
-  Value float64
-}
-
-func (c *Card) Refill(value float64) bool {
-  c.Value += value
-  return true
+	Value float64
 }
--- card/card_test.go
+++ card/card_test.go
@@ -1,15 +1,22 @@
 package metrocard
 
 import (
-  "testing"
+	"testing"
 )
+
 var value float64 = 20.00
 
-func TestRefill(t *testing.T) {
-  card := Card{Value: value}
-  card.Refill(value)
-  if card.Value != (value * 2) {
-    t.Error("Expected card to have a value of", (value * 2), "got", card.Value)
-  }
+func TestNew(t *testing.T) {
+	card := Card.New(value)
+	if card.Value != (value + (value * 0.5)) {
+		t.Error("Expected card to have a value of", (value + (value * 0.5)), "got", card.Value)
+	}
 }
 
+func TestRefill(t *testing.T) {
+	card := Card{Value: value}
+	card.Refill(value)
+	if card.Value != (value * 2) {
+		t.Error("Expected card to have a value of", (value * 2), "got", card.Value)
+	}
+}

2e2f2e79f1a77784dcc43c92384fae30bfe7f4d6
Dinshaw Gobhai <gobhai@gmail.com>
2014-01-31 02:09:21 -0500

    Starting main calc

---
+++ metrocard.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// How much left or your card?
+	card := kiosk.NewCard(starting_balance)
+	for i := 0.01; Mod(card.Value, ride_cost) == 0; i++ {
+
+	}
+	// If you add $xx you will have a zero balance after yy rides.
+	fmt.Printf("%v ride cost\n")
+}

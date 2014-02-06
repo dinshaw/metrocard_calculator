dfcefc41b578258b29c79a53c8918b4e974d3282
Dinshaw <gobhai@gmail.com>
2014-02-05 21:28:46 -0500

    add money type

--- card/card.go
+++ card/card.go
@@ -1,8 +1,8 @@
 package metrocard
 
 import (
-  "math"
-  "fmt"
+	// "fmt"
+	"math"
 )
 
 var ride_cost int = 150
@@ -10,53 +10,52 @@ var bonus_percent int = 5
 var min_increment int = 5
 
 type card struct {
-	value int
-  RidesRemaining int
+	value          int
+	RidesRemaining int
 }
 
 func NewCard(value int) *card {
-  card := new(card)
-  card.value = valueWithBonus(value)
-  return card
+	card := new(card)
+	card.value = valueWithBonus(value)
+	return card
 }
 
 func (c *card) Value() int {
-  return int(c.value)
+	return int(c.value)
 }
 
 func valueWithBonus(value int) int {
-  bonus := int(math.Floor(float64(value) * float64(bonus_percent) * float64(0.01)))
-  value_with_bonus := value + bonus
-  return value_with_bonus
+	bonus := int(math.Floor(float64(value) * float64(bonus_percent) * float64(0.01)))
+	value_with_bonus := value + bonus
+	return value_with_bonus
 }
 
 func ExistingCard(value int) *card {
-  card := new(card)
-  card.value = value
-  return card
+	card := new(card)
+	card.value = value
+	return card
 }
 
 func (c *card) RefillCard(value int) bool {
-  c.value += valueWithBonus(value)
-  return true
+	c.value += valueWithBonus(value)
+	return true
 }
 
-func(c card) Calculate() (int, int) {
-  initial_value := c.value
-  value_to_add := 0
-  for i := 0; math.Mod(float64(c.value), float64(ride_cost)) != 0; i += min_increment {
-    c.value = initial_value
+func (c card) Calculate() (int, int) {
+	initial_value := c.value
+	value_to_add := 0
+	for i := 0; math.Mod(float64(c.value), float64(ride_cost)) != 0; i += min_increment {
+		c.value = initial_value
 
-    fmt.Printf("Starting Card value %v\n", c.value)
-    fmt.Printf("Adding %v, adjusted to %v\n", i, valueWithBonus(i))
+		// fmt.Printf("Starting Card value %v\n", c.value)
+		// fmt.Printf("Adding %v, adjusted to %v\n", i, valueWithBonus(i))
 
-    c.RefillCard(i)
-    value_to_add = i
+		c.RefillCard(i)
+		value_to_add = i
 
-    fmt.Printf("New Card value is %v\n", c.value)
-    fmt.Printf("Mod %v\n", math.Mod(float64(c.value), float64(ride_cost)))
-    fmt.Printf("=====================\n")
-  }
-  return value_to_add, (c.Value() / ride_cost)
+		// fmt.Printf("New Card value is %v\n", c.value)
+		// fmt.Printf("Mod %v\n", math.Mod(float64(c.value), float64(ride_cost)))
+		// fmt.Printf("=====================\n")
+	}
+	return value_to_add, (c.Value() / ride_cost)
 }
-
--- metrocard.go
+++ metrocard.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/dinshaw/metrocard/card"
+	"log"
+	"strconv"
 )
-var ride_cost float64 = 1.50
+
+type money int
+
+func (m *money) String() string {
+	// return fmt.Sprintf("$%.2d", *m)
+	return "aoeu"
+}
+
+func (m *money) Set(value string) error {
+	float, err := strconv.ParseFloat(value, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	*m = money(float * 100)
+	return nil
+}
+
+var moneyFlag money
+var value_to_add money
+
+func init() {
+	flag.Var(&moneyFlag, "existing", "How much is currently on your Metrocard")
+}
 
 func main() {
-	card := metrocard.ExistingCard(starting_balance)
-  card.Calculate
-	fmt.Printf(
-    "If you add $%v you will have a zero balance after %v rides.\n"
-    card.AmountToAdd,
-    card.RidesRemaining
-  )
+	flag.Parse()
+	c := metrocard.ExistingCard(int(moneyFlag))
+	value_to_add, rides_remaining := c.Calculate()
+	fmt.Printf("If you add %v you will have a zero balance after %v rides.\n", money(value_to_add), rides_remaining)
+
 }

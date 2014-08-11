6f3974e9f0c3c31369efa45a316d0aa9c415ceff
Dinshaw <gobhai@gmail.com>
2014-08-11 17:18:16 -0400

    Stuff

--- money/money.go
+++ money/money.go
@@ -3,28 +3,30 @@ package metrocard
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"strconv"
 )
 
 type Money int64
 
-func (m *Money) String() string {
-	return fmt.Sprintf("$%d.%02d", int64(*m)/100, int64(*m)%100)
-}
+// func (m *Money) String() string {
+// 	return fmt.Sprintf("$%d.%02d", int64(*m)/100, int64(*m)%100)
+// }
 
-func (m *Money) Set(value string) error {
-	switch t := value.(type) {
+func (m *Money) Set(value interface{}) error {
+	fmt.Printf("aoeu", reflect.TypeOf(value))
+	switch value.(type) {
 	case string:
-		float, err := strconv.ParseFloat(value, 64)
+		fmt.Print("String")
+		float, err := strconv.ParseFloat(value.(string), 64)
 		*m = Money(float * 100)
 		// case float64:
 		// 	*m = Money(value * 100)
 		// case int:
+		if err != nil {
+			log.Fatal(err)
+		}
 		// 	*m = Money(value)
-	}
-
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	return nil
--- money/money_test.go
+++ money/money_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 )
 
-var m Money = 150
+var m Money
 
-func TestString(t *testing.T) {
-	if m.String() != "$1.50" {
-		t.Error("Expected $1.50, got ", m.String())
-	}
-}
+// // func TestString(t *testing.T) {
+// 	m.set("1.00")
+// 	if m.String() != "$1.50" {
+// 		t.Error("Expected $1.50, got ", m.String())
+// 	}
+// }
 
 func TestSetTakesAString(t *testing.T) {
 	m.Set("1.00")
@@ -19,9 +20,9 @@ func TestSetTakesAString(t *testing.T) {
 	}
 }
 
-func TestSetTakesAFloat(t *testing.T) {
-	m.Set(1.00)
-	if m != 100 {
-		t.Error("Expected 100, got ", m)
-	}
-}
+// func TestSetTakesAFloat(t *testing.T) {
+// 	m.Set(1.00)
+// 	if m != 100 {
+// 		t.Error("Expected 100, got ", m)
+// 	}
+// }
